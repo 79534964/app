2c3f612e59ed8553cbaaf61d32b92e40a5c3e692
wangzhuoyuan <79534964@qq.com>
2017-02-13 16:30:43 +0800

    2.13

--- oa-pc/src/vuex/module.js
+++
@@ -1,7 +0,0 @@
-export default {
-  getDateTime(date, type) {
-    let str = `${date.getFullYear()}-${(date.getMonth() + 1)}-${date.getDate()}`;
-    str = type === 'start' ? `${str} 00:00` : `${str} 23:59`;
-    return str;
-  }
-};
